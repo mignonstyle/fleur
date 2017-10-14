c08d66cdad064db5185b9578da671faa4cf4b22a
mignonstyle <mignonxstyle@gmail.com>
2017-10-14 09:59:16 +0900

    modify functions.php setup()

--- functions.php
+++ functions.php
@@ -11,8 +11,7 @@
  * Sets up theme defaults and registers support
  */
 function fleur_setup() {
-	// Make theme available for translation.
-	// Translations can be filed in the /languages/ directory.
+	// Make theme available for translation. Translations can be filed in the /languages/ directory.
 	load_theme_textdomain( 'fleur', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to head.
@@ -43,8 +42,5 @@ function fleur_setup() {
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	// This theme styles the visual editor to resemble the theme style, specifically font, colors, and column width.
-	//add_editor_style( array( 'assets/css/editor-style.css', fleur_fonts_url() ) );
 }
 add_action( 'after_setup_theme', 'fleur_setup' );
