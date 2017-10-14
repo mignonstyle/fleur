d22f9c14c8a682220fbba5d6c1f59727885a7243
mignonstyle <mignonxstyle@gmail.com>
2017-10-14 15:38:17 +0900

    modify index.php

--- index.php
+++ index.php
@@ -1,10 +1,41 @@
 <?php
 /**
- * File name index.php
+ * The main template file
+ *
+ * @package   fleur
+ * @copyright Mignon Style
+ * @version   1.0
  */
 
-class Main {
-	function _construct() {
-		echo 'hoge';
-	}
-}
+get_header(); ?>
+
+<div class="l-wrap">
+	<div id="primary" class="l-content-area">
+		<main id="main" class="l-site-main" role="main">
+
+			<?php if ( have_posts() ) :
+
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'template-parts/post/content' );
+
+				endwhile;
+
+				// pagination of posts.
+				get_template_part( 'template-parts/navigation/pagination', 'posts' );
+
+			else :
+
+				get_template_part( 'template-parts/post/content', 'none' );
+
+			endif;
+			?>
+
+		</main><!-- /#main -->
+	</div><!-- /#primary -->
+
+	<?php get_sidebar(); ?>
+</div><!-- /.l-wrap -->
+
+<?php get_footer(); ?>
