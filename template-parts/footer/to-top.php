d202090af314cbcca8839aa73018346dbab93fb6
mignonstyle <mignonxstyle@gmail.com>
2017-10-14 20:56:41 +0900

    add back to top

---
+++ template-parts/footer/to-top.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Back to the top of the page button.
+ *
+ * @package   fleur
+ * @copyright Mignon Style
+ * @version   1.0
+ */
+
+?>
+
+<div class="c-page-top" aria-hidden="true">
+	<a href="#body">
+		<i class="fa fa-angle-up" aria-hidden="true"></i><?php esc_html_e( 'Back To Top', 'fleur' ); ?>
+	</a>
+</div><!-- /.c-page-top -->
