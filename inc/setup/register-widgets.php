685f022f854b140ed301a95a27eb08f0c9947bdb
mignonstyle <mignonxstyle@gmail.com>
2017-10-14 16:30:55 +0900

    modify register-widgets class name

--- inc/setup/register-widgets.php
+++ inc/setup/register-widgets.php
@@ -17,9 +17,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Sidebar Top', 'fleur' ),
 		'id'            => 'sidebar-top-widget-area',
 		'description'   => __( 'Add widgets here will display it at the top of the sidebar.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 
@@ -27,9 +27,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Sidebar', 'fleur' ),
 		'id'            => 'sidebar-widget-area',
 		'description'   => __( 'Add widgets here will display it at the sidebar.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 
@@ -37,9 +37,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Sidebar Bottom', 'fleur' ),
 		'id'            => 'sidebar-bottom-widget-area',
 		'description'   => __( 'Add widgets here will display it at the bottom of the sidebar.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 
@@ -47,9 +47,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Archive sidebar', 'fleur' ),
 		'id'            => 'archive-sidebar-widget-area',
 		'description'   => __( 'Add widgets here will display it at the sidebar in archive page.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 
@@ -57,9 +57,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Content Top', 'fleur' ),
 		'id'            => 'content-top-widget-area',
 		'description'   => __( 'Add widgets here will display it at the top of the content area.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 
@@ -67,9 +67,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Content Bottom', 'fleur' ),
 		'id'            => 'content-bottom-widget-area',
 		'description'   => __( 'Add widgets here will display it at the bottom of the content area.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 
@@ -77,9 +77,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Footer Top', 'fleur' ),
 		'id'            => 'footer-top-widget-area',
 		'description'   => __( 'Add widgets here will display it at the top of the footer.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 
@@ -87,9 +87,9 @@ function fleur_widgets_init() {
 		'name'          => __( 'Footer Bottom', 'fleur' ),
 		'id'            => 'footer-bottom-widget-area',
 		'description'   => __( 'Add widgets here will display it at the bottom of the footer.', 'fleur' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_title'  => '<h2 class="c-widget__title">',
 		'after_title'   => '</h2>',
 	) );
 }
