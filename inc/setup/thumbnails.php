1bfa04491b2b82e0862418c72e890d2933800abd
mignonstyle <mignonxstyle@gmail.com>
2017-10-14 13:19:30 +0900

    modify thumbnails

--- inc/setup/thumbnails.php
+++ inc/setup/thumbnails.php
@@ -11,9 +11,9 @@
  * Enable support for Post Thumbnails on posts and pages.
  *
  * @return void
- * @see http://codex.wordpress.org/Post_Thumbnails
- * @see http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
- * @see http://codex.wordpress.org/Function_Reference/add_image_size
+ * @link https://developer.wordpress.org/reference/functions/add_theme_support/#post-thumbnails
+ * @link https://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
+ * @link https://developer.wordpress.org/reference/functions/add_image_size/
  */
 function fleur_setup_thumbnails() {
 	add_theme_support( 'post-thumbnails' );
