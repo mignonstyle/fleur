43f4cc5ea9da9cf3e1609cd0476a6d0cb57adeac
mignonstyle <mignonxstyle@gmail.com>
2017-10-15 14:03:18 +0900

    modify index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ get_header(); ?>
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
 
-					get_template_part( 'template-parts/post/content' );
+					get_template_part( 'template-parts/content/content' );
 
 				endwhile;
 
@@ -27,7 +27,7 @@ get_header(); ?>
 
 			else :
 
-				get_template_part( 'template-parts/post/content', 'none' );
+				get_template_part( 'template-parts/content/content', 'none' );
 
 			endif;
 			?>
